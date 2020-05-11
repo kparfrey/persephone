ab2d6b78abaacef5c764e73e693e3db4ccb07de9
Kyle <kyle@narwhal>
2020-05-11 14:46:42 -0400

    Added double plot to geometry.py.

--- geometry.py
+++ geometry.py
@@ -7,10 +7,12 @@ def disc_to_physical(r, theta, Rm, Zm):
     R = 0.0
     Z = 0.0
     M = Rm.shape[0]
-    for m in range(M):
+    for m in range(1, M):
         R += Rm[m] * np.cos(m*theta)
         Z += Zm[m] * np.sin(m*theta)
-    return (r*R,r*Z)
+    Rfinal = r*R + Rm[0]
+    Zfinal = r*Z
+    return (Rfinal, Zfinal)
 
 
 def square(theta, h):
@@ -25,7 +27,7 @@ def square(theta, h):
     return r
 
 
-def corner_lines(t_outer, h):
+def corner_lines(t_outer, h, Rm, Zm):
     Nr = 70
     R0s = np.array([h, -h, -h,  h])
     Z0s = np.array([h,  h, -h, -h])
@@ -62,11 +64,12 @@ M = 5
 Rm = np.zeros(M)
 Zm = np.zeros(M)
 
+Rm[0] = 2.0
 Rm[1] = 1.0
 Zm[1] = 2.0
-Rm[2] = 0.4
+Rm[2] = 0.2
 Zm[2] = 0.2
-Rm[3] = -0.1
+Rm[3] = 0.0
 Zm[3] = 0.0
 
 h = 0.4  # Inner square half-width on unit disc
@@ -78,14 +81,14 @@ Ro, Zo = disc_to_physical(circle(theta),    theta, Rm, Zm)
 Rs, Zs = disc_to_physical(square(theta, h), theta, Rm, Zm)
 
 ### Quad corners on the outer boundary
-#outer_corners  = np.array([pi/4, 3*pi/4, 5*pi/4, 7*pi/4]) ### Default: same as square
-outer_corners  = np.array([pi/4, 0.85*pi, 1.15*pi, 7*pi/4])
+outer_corners  = np.array([pi/4, 3*pi/4, 5*pi/4, 7*pi/4]) ### Default: same as square
+#outer_corners  = np.array([pi/4, 0.85*pi, 1.15*pi, 7*pi/4])
 
 ### Outer quad corners in physical space
 Rc, Zc = disc_to_physical(circle(outer_corners), outer_corners, Rm, Zm)
 
 ### Lines connecting inner square to outer quad corners, physical space
-Rcl, Zcl = corner_lines(outer_corners, h)
+Rcl, Zcl = corner_lines(outer_corners, h, Rm, Zm)
 
 
 
@@ -93,11 +96,25 @@ plt.figure()
 ax = plt.gca()
 ax.set_aspect('equal')
 
-plt.plot(Ro, Zo)
-plt.plot(Rs, Zs)
+### Plot unit disc without transformation ###
+Rm0 = Zm0 = np.array([0,1])
+Ro0, Zo0 = disc_to_physical(circle(theta),    theta, Rm0, Zm0)
+Rs0, Zs0 = disc_to_physical(square(theta, h), theta, Rm0, Zm0)
+Rc0, Zc0 = disc_to_physical(circle(outer_corners), outer_corners, Rm0, Zm0)
+Rcl0, Zcl0 = corner_lines(outer_corners, h, Rm0, Zm0)
+plt.plot(Ro0, Zo0, 'C0')
+plt.plot(Rs0, Zs0, 'C1')
+for i in [0,1,2,3]:
+    plt.plot(Rcl0[i,:], Zcl0[i,:], 'C2')
+plt.plot(Rc0, Zc0, '.', color='C3')
+
+### Plot transformed cross-section ##########
+plt.plot(Ro, Zo, 'C0')
+plt.plot(Rs, Zs, 'C1')
 
 for i in [0,1,2,3]:
-    plt.plot(Rcl[i,:], Zcl[i,:], 'k')
-plt.plot(Rc, Zc, '.', color='r')
+    plt.plot(Rcl[i,:], Zcl[i,:], 'C2')
+plt.plot(Rc, Zc, '.', color='C3')
 
 plt.show()
+#plt.savefig('/Users/kyle/Desktop/mapping_simple.pdf')
