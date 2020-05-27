3fb32cd0885cb6a257cb131f11d6a6e785a7180e
Kyle <kyle@narwhal>
2020-05-27 17:19:59 -0400

    Added metric.hpp and .cpp for previous commit.

---
+++ metric.cpp
@@ -0,0 +1,98 @@
+#include "metric.hpp"
+#include "kernels.hpp"
+
+void Metric::allocate(int N)
+{
+    /* For now, assume that N is the no. of solution points per block,
+     * and that no metric quantities on flux points are required. */
+
+    for (int i: dirs)
+        for (int j: dirs)
+        {
+               J[i][j]  = kernels::alloc(N);
+            Jinv[i][j]  = kernels::alloc(N);
+               g[i][j]  = kernels::alloc(N);
+            ginv[i][j]  = kernels::alloc(N);
+            gphys[i][j] = kernels::alloc(N);
+        }
+
+    rdetg = kernels::alloc(N);
+
+    return;
+}
+
+
+/* Simplified version that assumes basic Cartesian shape */
+void Metric::setup(int Nelem[3], int Ns_block, real_t corners[8][3])
+{
+    allocate(Ns_block);
+
+
+    real_t dr_elemblock[3];
+    real_t dr_elem[3];
+
+    dr_elemblock[0] = corners[1][0] - corners[0][0];
+    dr_elemblock[1] = corners[3][1] - corners[0][1];
+    dr_elemblock[2] = corners[4][2] - corners[0][2];
+
+    for (int i: dirs)
+        dr_elem[i] = dr_elemblock[i] / Nelem[i];
+
+    /* Can leave all off-diagonal components as zero */
+    for (int d: dirs)
+        for (int i = 0; i < Ns_block; ++i) // All elems identical
+        {
+            J[d][d][i]    = dr_elem[d]/2.0; // Since length of ref element is 2
+            Jinv[d][d][i] = 1.0 / J[d][d][i];
+            gphys[d][d][i] = 1.0;
+        }
+
+    transform_twoTensor(gphys, g, Ns_block, phys2ref, covariant);
+
+    /*
+    for (int a: dirs)
+    for (int b: dirs)
+        for (int i: dirs)
+        for (int j: dirs)
+            for (int n = 0; n < Ns_block; ++n) 
+                g[a][b][n] = J[a][i][n] * J[b][j][n] * gphys[i][j][n];
+    */
+            
+
+    return;
+}
+
+
+/* Do a general coordinate transformation for a two-tensor between reference
+ * and physical coordinates */
+void Metric::transform_twoTensor(real_t* T_in[3][3], real_t* T_out[3][3], int N,
+                                 CoordTransDir ctd, Components c)
+{
+    /* Pointer to a 3x3 array of pointers to real_t... */
+    real_t* (*V)[3][3];
+
+    switch(c)
+    {
+        case covariant:
+            if (ctd == phys2ref)
+                V = &J;
+            else
+                V = &Jinv;
+            break;
+        case contravariant:
+            if (ctd == phys2ref)
+                V = &Jinv;
+            else
+                V = &J;
+            break;
+    }
+    
+    for (int a: dirs)
+    for (int b: dirs)
+        for (int i: dirs)
+        for (int j: dirs)
+            for (int n = 0; n < N; ++n) 
+                T_out[a][b][n] = (*V)[a][i][n] * (*V)[b][j][n] * T_in[i][j][n];
+
+    return;
+}
---
+++ metric.hpp
@@ -0,0 +1,42 @@
+#ifndef METRIC_HPP
+#define METRIC_HPP
+
+#include "common.hpp"
+#include "kernels.hpp"
+
+/* LOGICAL coords are the 3D Cartesian coordinates local to the
+ * reference element --- i.e. x[0-2] in [-1,1]
+ * Synonymous with COMPUTATIONAL/REFERENCE/ELEMENTAL coordinates */
+
+/* For now, all metric arrays are on solution points only 
+ * --- assume that all metric operations can be performed before
+ *     the transform to flux points */
+
+
+class Metric
+{
+    public:
+
+    /* Transforms between physical and reference coords */
+    real_t*    J[3][3]; // d(PHYSICAL)/d(LOGICAL)
+    real_t* Jinv[3][3]; // d(LOGICAL)/d(PHYSICAL)
+
+    /* Reference coords */
+    real_t*    g[3][3]; // Spatial 3-metric (dual/covariant)
+    real_t* ginv[3][3]; // Inverse 3-metric (vector/contravariant)
+    real_t* rdetg;      // Square root of 3-metric determinant
+
+    /* Physical coords */
+    real_t* gphys[3][3]; // Spatial 3-metric, in chosen physical coords
+
+
+    /* Methods */
+    void allocate(int N);
+    void setup(int Nelem[3], int Ns_block, real_t corners[8][3]);
+    void transform_twoTensor(real_t* T_in[3][3], real_t* T_out[3][3], int N,
+                             CoordTransDir ctd=phys2ref, Components c=covariant);
+    
+};
+
+
+#endif
