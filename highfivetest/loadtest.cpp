47ad2123d1c16d755047b194393557550a34750a
Kyle <kyle@narwhal>
2021-08-09 17:09:25 +0100

    Added new mini program in highfivetest to confirm loading 1D and multi-D vectors from HDF5 files, using example DESC output.

---
+++ highfivetest/loadtest.cpp
@@ -0,0 +1,38 @@
+#include <highfive/H5Easy.hpp>
+#include <iostream>
+
+int main()
+{
+    using std::vector;
+    using std::cout;
+    using std::endl;
+
+
+    H5Easy::File dfile("../../DESC/examples/DESC/DSHAPE_output.h5", H5Easy::File::ReadOnly);
+
+    vector<double> R_lmn;
+
+    R_lmn = H5Easy::load<vector<double>>(dfile, "/_equilibria/2/_R_lmn");
+
+    const int num_R_modes = R_lmn.size(); 
+    cout << "No. of modes in R function: " << num_R_modes << endl;
+
+    for (int i = 0; i < 10; ++i)
+        cout << "Coeff " << i << " :  " << R_lmn[i] << endl;
+
+
+    cout << "\n" << endl;
+
+    vector<vector<int>> R_modes;
+
+    R_modes = H5Easy::load<vector<vector<int>>>(dfile, "/_equilibria/2/_R_basis/_modes");
+
+    const int n0 = R_modes.size();
+    const int n1 = R_modes[0].size();
+
+    cout << "Mode array dimensions: " << n0 << " " << n1 << endl;
+    for (int i = 0; i < n0; ++i)
+        cout << "Coeff " << i << " :  " << R_modes[i][0] << " " << R_modes[i][1] << " " <<  R_modes[i][2] << endl;
+
+    return 1;
+}
