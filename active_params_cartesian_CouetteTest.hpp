c4bef741fc2db36270a3daeebf596d9486e346eb
Kyle <kyle@narwhal>
2024-04-08 10:37:44 -0400

    Added active_params file for the Couette test.

---
+++ active_params_cartesian_CouetteTest.hpp
@@ -0,0 +1,19 @@
+#include "common.hpp"
+#include "params_cartesian.hpp"
+
+/* For discussion of non-standard features see active_params_torus_0.hpp */
+
+static EqnSystem equations = navier_stokes;
+static BasicTimeMethod time_method = rk3_ssp;
+
+static int Nproc[3] = {1,2,1};
+static int Nelem[3] = {1,5,1};
+static int Ns[3]    = {1,5,1};
+
+static real_t cfl      = 0.5; 
+static real_t end_time = 1.0;
+static real_t dt_write = 0.01;
+
+static ParamsCartesian active_parameters(equations, time_method, 
+                                         Nproc, Nelem, Ns,
+                                         cfl, end_time, dt_write);
