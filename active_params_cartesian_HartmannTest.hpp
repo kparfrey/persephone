85f2d84a1e5085a70f2b456af3ac6d5df5399703
Kyle <kyle@narwhal>
2024-04-22 10:59:44 -0400

    Belatedly adding active_params file for the Hartmann flow test.

---
+++ active_params_cartesian_HartmannTest.hpp
@@ -0,0 +1,21 @@
+#include "common.hpp"
+#include "params_cartesian.hpp"
+
+/* For discussion of non-standard features see active_params_torus_0.hpp */
+
+static EqnSystem equations = mhd;
+static BasicTimeMethod time_method = rk3_ssp;
+
+static int Nproc[3] = {1,4,1};
+static int Nelem[3] = {1,8,1};
+static int Ns[3]    = {1,6,1};
+
+static real_t cfl      = 0.8; 
+static real_t end_time = 20.0;
+static real_t dt_write = 0.1;
+
+static bool periodic = false; // Non-periodic in the y (1) direction
+
+static ParamsCartesian active_parameters(equations, time_method, 
+                                         Nproc, Nelem, Ns,
+                                         cfl, end_time, dt_write, periodic);
